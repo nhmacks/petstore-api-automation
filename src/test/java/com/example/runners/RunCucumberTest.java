cc1143f2f44b8f35acce16eed3442386dfd25c8b
Nunez Huaranga <macksmiguel.nunezhuaranga@externos-pe.cencosud.com>
2026-01-08 16:05:28 -0500

    fix

--- src/test/java/com/example/runners/RunCucumberTest.java
+++ src/test/java/com/example/runners/RunCucumberTest.java
@@ -1,10 +1,10 @@
 package com.example.runners;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
     features = "src/test/resources/features",
     plugin = {"pretty", "json:target/cucumber.json", "junit:target/cucumber.xml"},
